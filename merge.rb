5b9afbabfe4a9e89c5c82a721071376522632944
Alejandro <chapale2000@gmail.com>
2025-08-11 23:31:38 -0300

    project done

--- merge.rb
+++ merge.rb
@@ -1,30 +1,21 @@
 def merge_sort(arr)
-#[3,1,4,0]
-  if arr.length == 1
-    return arr
-  end
-
+  return arr if arr.length <= 1
   mid = arr.length / 2
-  a = merge_sort(arr[0..mid-1])
-  b = merge_sort(arr[mid..-1])
-  arr_sorted = []
+  left = merge_sort(arr[0..mid-1])
+  right = merge_sort(arr[mid..])
+  merge(left, right)
+end
 
-  while !a.empty? || !b.empty?
-    if a.empty?
-      arr_sorted << b.shift
-      next
-    elsif b.empty?
-      arr_sorted << a.shift
-      next
-    end
-    if a.first < b.first
-      arr_sorted << a.shift
-    else 
-      arr_sorted << b.shift
+def merge(left, right)
+  sorted = []
+  until left.empty? || right.empty?
+    if left.first < right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
     end
   end
-  arr_sorted
-
+  sorted.concat(left).concat(right)
 end
 
 #pito = [3, 2, 1, 13, 8, 5, 0, 1]
